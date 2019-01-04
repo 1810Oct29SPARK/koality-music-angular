b03a1a75dc908cc9979a291921e87d0df1808109
Eddy Soma <esoma92.dev@gmail.com>
2019-01-04 03:35:55 -0500

    create two feature modules

---
+++ src/app/publisher/publisher.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PublisherRoutingModule } from './publisher-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PublisherRoutingModule
+  ]
+})
+export class PublisherModule { }
