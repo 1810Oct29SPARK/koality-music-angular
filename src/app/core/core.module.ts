82b6cdf7f67392e2dd51cb8990d6901ee99184a1
Eddy Soma <esoma92.dev@gmail.com>
2019-01-10 00:01:33 -0500

    add bootstrap library

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -8,9 +11,18 @@ import { RegisterComponent } from './register/register.component';
 import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
-  declarations: [PageNotFoundComponent, LoginComponent, RegisterComponent, LandingComponent],
+  declarations: [
+    PageNotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
     CoreRoutingModule
   ]
 })
