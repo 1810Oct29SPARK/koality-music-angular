689fe7da51c47e501b9949ee7ab5111a595a65c6
Eddy Soma <esoma92.dev@gmail.com>
2019-01-14 03:26:42 -0500

    add publish track

--- src/app/publisher/publisher-publish-track/publisher-publish-track.component.ts
+++ src/app/publisher/publisher-publish-track/publisher-publish-track.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-publisher-publish-track',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublisherPublishTrackComponent implements OnInit {
 
-  constructor() { }
+  publishTrackForm: FormGroup;
+  publishFormControls: any;
+  audioName = 'Choose file';
+  audioType: string;
+  audioUrl: string;
+
+  constructor(public formBuilder: FormBuilder) {
+
+    this.publishTrackForm = this.formBuilder.group({
+      trackName: ['', Validators.required],
+      genre: ['', Validators.required],
+      composer: ['', Validators.required],
+      artist: ['', Validators.required],
+      unitPrice: ['', [Validators.required]],
+      audio: ['', Validators.required]
+    });
+
+  }
 
   ngOnInit() {
+    this.publishFormControls = this.publishTrackForm.controls;
+  }
+
+  loadAudioUrl(event: any) {
+    const reader = new FileReader();
+    const file = event.target.files[0];
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.audioName = file.name;
+      this.audioType = file.name.substring(file.name.length - 3);
+      this.audioUrl = reader.result.toString();
+    };
+  }
+
+  publishTrack() {
+    console.log('publish track form submitted');
+    console.log(this.publishTrackForm.value);
   }
 
 }
