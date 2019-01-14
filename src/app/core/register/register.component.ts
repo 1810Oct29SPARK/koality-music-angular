5f86857c69c909bc07eebbdb44bce6a25c78c0b7
Eddy Soma <esoma92.dev@gmail.com>
2019-01-13 21:40:11 -0500

    register form

--- src/app/core/register/register.component.ts
+++ src/app/core/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { GlobalVars } from '../../shared/global-vars';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  publisherRegisterForm: FormGroup;
+  customerRegisterForm: FormGroup;
+
+  musicGenres = GlobalVars.musicGenres;
+
+  constructor(public formBuilder: FormBuilder) {
+
+    this.publisherRegisterForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      companyName: ['', Validators.nullValidator],
+      username: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirmPassword: ['', Validators.nullValidator]
+    });
+
+    this.customerRegisterForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      favoriteGenre: ['Choose your favorite genre', Validators.nullValidator],
+      username: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirmPassword: ['', Validators.nullValidator]
+    });
+
+  }
 
   ngOnInit() {
+  }
+
+  customerRegister() {
+    console.log('customer register form submitted');
+    console.log(this.customerRegisterForm.value);
+  }
+
+  publisherRegister() {
+    console.log('publisher register form submitted');
+    console.log(this.publisherRegisterForm.value);
   }
 
 }
